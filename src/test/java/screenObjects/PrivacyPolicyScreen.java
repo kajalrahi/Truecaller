8273b2a97758e9872461835abd744eadff0833ef
kajalrahi <145103721+kajalrahi@users.noreply.github.com>
2024-01-07 21:25:37 +0100

    Update PrivacyPolicyScreen.java

--- src/test/java/screenObjects/PrivacyPolicyScreen.java
+++ src/test/java/screenObjects/PrivacyPolicyScreen.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utility.Helper;
 
-public class PrivacyPolicyScreen extends DefaultDriver{
+public class PrivacyPolicyScreen {
     AndroidDriver driver;
     private final String privacyScreenTitle = "//android.widget.TextView[@resource-id=\"com.truecaller:id/privacyPolicyTitle\"]";
     private final String agreeAndContinueButton = "//android.widget.Button[@resource-id=\"com.truecaller:id/nextButton\"]";
